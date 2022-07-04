6038d94c0b4db9e4c42b369e4f363703c06a2867
Alejandro <101234434+Alejandro88G@users.noreply.github.com>
2022-07-03 23:38:47 -0300

    Actualizacion de carpetas
    Actualizacion de datos.

---
+++ Proyecto_Integrador_AJG/Back_End/src/main/java/com/portfolio/ajg/AjgApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.ajg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjgApplication.class, args);
+	}
+
+}
---
+++ Proyecto_Integrador_AJG/Back_End/src/test/java/com/portfolio/ajg/AjgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.ajg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
